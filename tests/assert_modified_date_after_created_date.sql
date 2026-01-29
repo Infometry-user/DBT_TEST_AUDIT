0013a2e13a4da2c26d378cf6ae9de1f7b9ad2594
Adithi S <drithibhardwaj28@gmail.com>
2026-01-29 11:05:49 +0000

    deleted 3 files

--- tests/assert_modified_date_after_created_date.sql
+++
@@ -1,9 +0,0 @@
-
-
-select
-    CUSTOMER_ID,
-    CREATED_DATE,
-    MODIFIED_DATE
-from {{ ref('DIM_CUSTOMERS') }}
-where MODIFIED_DATE is not null 
-  and MODIFIED_DATE < CREATED_DATE
--- tests/assert_no_future_registration_dates.sql
+++
@@ -1,8 +0,0 @@
--- This test fails if any customer has a registration date 
--- later than the current date in Snowflake.
-
-select
-    CUSTOMER_ID,
-    REGISTRATION_DATE
-from {{ ref('DIM_CUSTOMERS') }}
-where REGISTRATION_DATE > CURRENT_DATE()
